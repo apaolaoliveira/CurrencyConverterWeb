18d84649406feea0d7d61bdf79f10bdf0807864d
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-03-17 02:04:55 -0300

    fea load country's flag

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,6 +1,8 @@
 class CurrencyConverter {
-  apiRequest = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/`
+  apiExchangeRate = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/`;
+  apiFlags = 'https://flagsapi.com/';
 
+  currenciesSelects = document.querySelectorAll('select');	
   fromCurrency = document.querySelector('#from');
   toCurrency = document.querySelector('#to');
   amount = document.querySelector('#amount');
@@ -9,17 +11,12 @@ class CurrencyConverter {
   result = document.querySelector('#result');
 
   constructor(){
-    this.addDropdown();
-
-    // default 
-    this.fromCurrency.value = 'USD';
-    this.toCurrency.value = 'BRL';
-    this.result.innerText = this.ConvertCurrencies(this.amount.value);
-
-    this.btnConvert.addEventListener('click', () => this.ConvertCurrencies(this.amount.value));
+    this.countriesDropdown();
+    this.defaultValues();    
+    this.addEventListeners();
   }
 
-  addDropdown() {
+  countriesDropdown() {
     for (const currency in countryList) {
       const option = document.createElement("option");
       option.value = currency;
@@ -29,13 +26,29 @@ class CurrencyConverter {
     }
   }
 
-  ConvertCurrencies(amount) {
+  defaultValues(){
+    this.fromCurrency.value = 'USD';
+    this.toCurrency.value = 'BRL';
+    this.result.innerText = this.convertCurrencies(this.amount.value);
+  }
+
+  addEventListeners(){
+    this.btnConvert.addEventListener('click', () => {
+      this.convertCurrencies(this.amount.value);
+    });
+
+    this.currenciesSelects.forEach(select => {
+      select.addEventListener('change', e => this.loadFlag(e.target));
+    });
+  }
+
+  convertCurrencies(amount) {
     if(amount === '' || amount === '0') {
       this.amount.value = '1';
       amount = '1';
     }
 
-    fetch(`${this.apiRequest}${this.fromCurrency.value}`)
+    fetch(`${this.apiExchangeRate}${this.fromCurrency.value}`)
       .then((res) => res.json())
       .then((data) => {
         let exchangeRate = data.conversion_rates[this.toCurrency.value];
@@ -47,6 +60,16 @@ class CurrencyConverter {
 
         this.result.innerText = `${formattedAmount} = ${formattedConversion}`;
       });
+  }
+
+  loadFlag(element) {
+    console.log('entrei' + element)
+    for (const code in countryList){
+      if(code == element.value) {
+        let imgTag = element.parentElement.querySelector('img');
+        imgTag.src = `${this.apiFlags}${countryList[code]}/flat/64.png`;
+      }
+    }
   }
 }
 
