f8af32844d27772f18988d7a53895885dcf855ee
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-03-17 01:03:31 -0300

    add currency conversion

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,14 +1,21 @@
 class CurrencyConverter {
-  request = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`
+  apiRequest = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/`
 
-  selects = document.querySelectorAll('select');
-  from = document.querySelector('#from');
-  to = document.querySelector('#to');
+  fromCurrency = document.querySelector('#from');
+  toCurrency = document.querySelector('#to');
+  amount = document.querySelector('#amount');
+
+  btnConvert = document.querySelector('#convert');
+  result = document.querySelector('#result');
 
   constructor(){
     this.addDropdown();
-    this.from.value = 'USD';
-    this.to.value = 'BRL';
+
+    // default 
+    this.fromCurrency.value = 'USD';
+    this.toCurrency.value = 'BRL';
+
+    this.btnConvert.addEventListener('click', () => this.ConvertCurrencies(this.amount.value));
   }
 
   addDropdown() {
@@ -16,9 +23,24 @@ class CurrencyConverter {
       const option = document.createElement("option");
       option.value = currency;
       option.text = currency;
-      this.from.add(option.cloneNode(true));
-      this.to.add(option);
+      this.fromCurrency.add(option.cloneNode(true));
+      this.toCurrency.add(option);
     }
+  }
+
+  ConvertCurrencies(amount) {
+    if(amount === '' || amount === '0') {
+      this.amount.value = '1';
+      amount = '1';
+    }
+
+    fetch(`${this.apiRequest}${this.fromCurrency.value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        let conversion = Number(amount) * data.conversion_rates[this.toCurrency.value];
+        this.result.innerText = 
+          `${amount} ${this.fromCurrency.value} = ${conversion} ${this.toCurrency.value}`;
+      });
   }
 }
 
